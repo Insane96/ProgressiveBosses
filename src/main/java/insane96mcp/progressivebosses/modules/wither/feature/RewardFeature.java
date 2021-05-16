5ed2573458b6e0051532bd7f8024b844fefc2bd0
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-05-16 16:40:24 +0200

    Increased Shards

--- src/main/java/insane96mcp/progressivebosses/modules/wither/feature/RewardFeature.java
+++ src/main/java/insane96mcp/progressivebosses/modules/wither/feature/RewardFeature.java
@@ -29,6 +29,7 @@ public class RewardFeature extends Feature {
 
 	private static final List<String> dropsListDefault = Arrays.asList("progressivebosses:nether_star_shard,1,2,2,MINIMUM,SCALING",
 			"progressivebosses:nether_star_shard,2,4,4,MINIMUM,SCALING",
+			"progressivebosses:nether_star_shard,3,6,8,MINIMUM,SCALING",
 			"progressivebosses:nether_star_shard,4,8,8,MINIMUM,SCALING");
 
 	public double bonusExperience = 20d;
