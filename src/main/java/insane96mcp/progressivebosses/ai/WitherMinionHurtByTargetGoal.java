da264bec1009e0e934f0bf5afa1b4a8769b5b48d
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-05-23 10:59:19 +0200

    Wither Minions no longer attack other Wither Minions

--- src/main/java/insane96mcp/progressivebosses/ai/WitherMinionHurtByTargetGoal.java
+++ src/main/java/insane96mcp/progressivebosses/ai/WitherMinionHurtByTargetGoal.java
@@ -5,12 +5,13 @@ import net.minecraft.entity.EntityPredicate;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.boss.WitherEntity;
+import net.minecraft.entity.monster.WitherSkeletonEntity;
 
 import java.util.function.Predicate;
 
 public class WitherMinionHurtByTargetGoal extends HurtByTargetGoal {
 
-    private static final Predicate<LivingEntity> NON_WITHER = livingEntity -> !(livingEntity instanceof WitherEntity);
+    private static final Predicate<LivingEntity> NON_WITHER = livingEntity -> !(livingEntity instanceof WitherEntity) && !(livingEntity instanceof WitherSkeletonEntity);
     private static final EntityPredicate TARGETING_CONDITIONS = (new EntityPredicate()).setLineOfSiteRequired().setUseInvisibilityCheck().setCustomPredicate(NON_WITHER);
 
     public WitherMinionHurtByTargetGoal(CreatureEntity p_i50317_1_, Class<?>... p_i50317_2_) {
