c53f0338cc224570cd43e76e2000b3e9c74f40b0
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-06-04 08:03:30 +0200

    Added damage reduction before half health 1% per difficulty up to 20% Decreased damage reduction on half health (4% -> 2.5%) Increased max damage reduction on half health (35% -> 40%)

--- src/main/java/insane96mcp/progressivebosses/modules/wither/feature/ResistancesFeature.java
+++ src/main/java/insane96mcp/progressivebosses/modules/wither/feature/ResistancesFeature.java
@@ -14,23 +14,33 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 @Label(name = "Resistances & Vulnerabilities", description = "Handles the Damage Resistances and Vulnerabilities")
 public class ResistancesFeature extends Feature {
 
-	private final ForgeConfigSpec.ConfigValue<Double> damageReductionPerDifficultyOnHalfHealthConfig;
-	private final ForgeConfigSpec.ConfigValue<Double> maxDamageReductionPerDifficultyOnHalfHealthConfig;
+	private final ForgeConfigSpec.ConfigValue<Double> damageReductionBeforeHalfHealthConfig;
+	private final ForgeConfigSpec.ConfigValue<Double> maxDamageReductionBeforeHalfHealthConfig;
+	private final ForgeConfigSpec.ConfigValue<Double> damageReductionOnHalfHealthConfig;
+	private final ForgeConfigSpec.ConfigValue<Double> maxDamageReductionOnHalfHealthConfig;
 	private final ForgeConfigSpec.ConfigValue<Double> magicDamageBonusConfig;
 
-	public double damageReductionPerDifficultyOnHalfHealth = 0.04d;
-	public double maxDamageReductionPerDifficultyOnHalfHealth = 0.35d;
+	public double damageReductionBeforeHalfHealth = 0.01d;
+	public double maxDamageReductionBeforeHalfHealth = 0.20d;
+	public double damageReductionOnHalfHealth = 0.025d;
+	public double maxDamageReductionOnHalfHealth = 0.40d;
 	public double magicDamageBonus = 200d;
 
 	public ResistancesFeature(Module module) {
 		super(Config.builder, module);
 		Config.builder.comment(this.getDescription()).push(this.getName());
-		damageReductionPerDifficultyOnHalfHealthConfig = Config.builder
-				.comment("Percentage Damage Resistance as the Wither drops below half health.")
-				.defineInRange("Damage reduction per Difficulty below half health", damageReductionPerDifficultyOnHalfHealth, 0d, 1d);
-		maxDamageReductionPerDifficultyOnHalfHealthConfig = Config.builder
-				.comment("Cap for 'Damage reduction per Difficulty below half health'")
-				.defineInRange("Max Damage reduction per Difficulty below half health", maxDamageReductionPerDifficultyOnHalfHealth, 0d, 1d);
+		damageReductionBeforeHalfHealthConfig = Config.builder
+				.comment("Percentage Damage Reduction while the Wither is above half health.")
+				.defineInRange("Damage reduction per Difficulty above half health", damageReductionBeforeHalfHealth, 0d, 1d);
+		maxDamageReductionBeforeHalfHealthConfig = Config.builder
+				.comment("Cap for 'Damage reduction per Difficulty above half health'")
+				.defineInRange("Max Damage reduction per Difficulty before half health", maxDamageReductionBeforeHalfHealth, 0d, 1d);
+		damageReductionOnHalfHealthConfig = Config.builder
+				.comment("Percentage Damage Reduction as the Wither drops below half health.")
+				.defineInRange("Damage reduction per Difficulty below half health", damageReductionOnHalfHealth, 0d, 1d);
+		maxDamageReductionOnHalfHealthConfig = Config.builder
+				.comment("Cap for 'Damage Reduction per Difficulty below half health'")
+				.defineInRange("Max Damage reduction per Difficulty below half health", maxDamageReductionOnHalfHealth, 0d, 1d);
 		magicDamageBonusConfig = Config.builder
 				.comment("Bonus magic damage based off missing health. 150 means that every 150 missing health the damage will be amplified by 100%. E.g. The difficulty = 0 Wither (with 300 max health) is at half health (so it's missing 150hp), on magic damage he will receive 'magic_damage * (missing_health / magic_damage_bonus + 1)' = 'magic_damage * (150 / 150 + 1)' = 'magic_damage * 2'.")
 				.defineInRange("Magic Damage Bonus", magicDamageBonus, 0d, 1024f);
@@ -40,17 +50,19 @@ public class ResistancesFeature extends Feature {
 	@Override
 	public void loadConfig() {
 		super.loadConfig();
-		this.damageReductionPerDifficultyOnHalfHealth = this.damageReductionPerDifficultyOnHalfHealthConfig.get();
-		this.maxDamageReductionPerDifficultyOnHalfHealth = this.maxDamageReductionPerDifficultyOnHalfHealthConfig.get();
+		this.damageReductionBeforeHalfHealth = this.damageReductionBeforeHalfHealthConfig.get();
+		this.maxDamageReductionBeforeHalfHealth = this.maxDamageReductionBeforeHalfHealthConfig.get();
+		this.damageReductionOnHalfHealth = this.damageReductionOnHalfHealthConfig.get();
+		this.maxDamageReductionOnHalfHealth = this.maxDamageReductionOnHalfHealthConfig.get();
 		this.magicDamageBonus = this.magicDamageBonusConfig.get();
 	}
 
 	@SubscribeEvent
-	public void onUpdate(LivingDamageEvent event) {
+	public void onWitherDamage(LivingDamageEvent event) {
 		if (!this.isEnabled())
 			return;
 
-		if ((this.damageReductionPerDifficultyOnHalfHealth == 0d || this.maxDamageReductionPerDifficultyOnHalfHealth == 0d) && this.magicDamageBonus == 0d)
+		if ((this.damageReductionOnHalfHealth == 0d || this.maxDamageReductionOnHalfHealth == 0d) && (this.damageReductionBeforeHalfHealth == 0d || this.maxDamageReductionBeforeHalfHealth == 0d) && this.magicDamageBonus == 0d)
 			return;
 
 		if (!(event.getEntity() instanceof WitherEntity))
@@ -63,14 +75,16 @@ public class ResistancesFeature extends Feature {
 			event.setAmount((event.getAmount() * (float) (missingHealth / (this.magicDamageBonus) + 1)));
 		}
 
-		//Handle Damage Reduction
-		if (!wither.isCharged())
-			return;
-
 		CompoundNBT tags = wither.getPersistentData();
 		float difficulty = tags.getFloat(Strings.Tags.DIFFICULTY);
 
-		float damageReduction = (float) Math.min(this.maxDamageReductionPerDifficultyOnHalfHealth, difficulty * this.damageReductionPerDifficultyOnHalfHealth);
+		//Handle Damage Reduction
+		float damageReduction;
+		if (!wither.isCharged())
+			damageReduction = (float) Math.min(this.maxDamageReductionBeforeHalfHealth, difficulty * this.damageReductionBeforeHalfHealth);
+		else
+			damageReduction = (float) Math.min(this.maxDamageReductionOnHalfHealth, difficulty * this.damageReductionOnHalfHealth);
+
 		event.setAmount(event.getAmount() * (1f - damageReduction));
 	}
 }
