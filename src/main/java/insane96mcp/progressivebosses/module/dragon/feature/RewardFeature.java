e0ad42248608146baa14fae7a284a8b81676c0f5
Insane96 <delvillano.alberto@gmail.com>
2022-05-06 11:43:44 +0200

    Removed old drops config option and added Inject default loot

--- src/main/java/insane96mcp/progressivebosses/module/dragon/feature/RewardFeature.java
+++ src/main/java/insane96mcp/progressivebosses/module/dragon/feature/RewardFeature.java
@@ -4,7 +4,6 @@ import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
 import insane96mcp.progressivebosses.ProgressiveBosses;
-import insane96mcp.progressivebosses.classutils.Drop;
 import insane96mcp.progressivebosses.setup.Config;
 import insane96mcp.progressivebosses.setup.Strings;
 import net.minecraft.core.BlockPos;
@@ -17,27 +16,20 @@ import net.minecraft.world.level.storage.loot.entries.LootTableReference;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Label(name = "Rewards", description = "Bonus Experience and Dragon Egg per player")
 public class RewardFeature extends Feature {
 
 	private final ForgeConfigSpec.ConfigValue<Double> bonusExperienceConfig;
 	private final ForgeConfigSpec.ConfigValue<Boolean> dragonEggPerPlayerConfig;
-	private final ForgeConfigSpec.ConfigValue<List<? extends String>> dropsListConfig;
-
-	private static final List<String> dropsListDefault = Arrays.asList("minecraft:enchanted_golden_apple,1,5,0.10,MINIMUM,SCALING");
+	private final ForgeConfigSpec.ConfigValue<Boolean> injectDefaultRewardsConfig;
 
 	public double bonusExperience = 4.5d;
 	public boolean dragonEggPerPlayer = true;
-	public ArrayList<Drop> dropsList;
+	public boolean injectDefaultRewards = true;
 
 	public RewardFeature(Module module) {
 		super(Config.builder, module);
@@ -48,20 +40,10 @@ public class RewardFeature extends Feature {
 		dragonEggPerPlayerConfig = Config.builder
 				.comment("If true whenever a player, that has never killed the dragon, kills the dragon a Dragon Egg ìì will drop. E.g. If 2 players kill the Dragon for the first time, she will drop 2 Dragon Eggs")
 				.define("Dragon Egg per Player", dragonEggPerPlayer);
-		dropsListConfig = Config.builder
-				.comment("""
-						A list of drops for the Dragons. Entry format: item,amount,difficulty_required,chance,difficulty_mode,chance_mode
-						item: item id
-						amount: amount
-						difficulty_required: the amount of difficulty required for the item to drop, works differently based on mode
-						chance: chance for the drop to happen, between 0 and 1
-						difficulty_mode:
-						* MINIMUM: will try to drop the item when the difficulty matches or is higher
-						* PER_DIFFICULTY: will try to drop the item once per difficulty (e.g. at difficulty 10, difficulty required 3, the chance to drop the item is tried 7 times)
-						chance_mode:
-						* FLAT: chance is the percentage chance for the item to drop if the difficulty criteria matches
-						* SCALING: each point of difficulty >= 'difficulty to drop the item' will be multiplied by the chance (e.g. difficulty 10, chance 2% and difficulty required 5, chance to drop the item will be chance * (difficulty - difficulty_required + 1) = 2% * (10 - 5 + 1) = 12%)""")
-				.defineList("Drops", dropsListDefault, o -> o instanceof String);
+		injectDefaultRewardsConfig = Config.builder
+				.comment("If true default mod drops are added to the Ender Dragon.\n" +
+						"Note that replacing the Ender Dragon loot table (e.g. via DataPack) will automatically remove the Injected loot.")
+				.define("Injecet Default Loot", this.injectDefaultRewards);
 		Config.builder.pop();
 	}
 
@@ -70,7 +52,7 @@ public class RewardFeature extends Feature {
 		super.loadConfig();
 		this.bonusExperience = this.bonusExperienceConfig.get();
 		this.dragonEggPerPlayer = this.dragonEggPerPlayerConfig.get();
-		this.dropsList = Drop.parseDropsList(this.dropsListConfig.get());
+		this.injectDefaultRewards = this.injectDefaultRewardsConfig.get();
 	}
 
 	@SubscribeEvent
@@ -124,26 +106,8 @@ public class RewardFeature extends Feature {
 	}
 
 	@SubscribeEvent
-	public void onDeath(LivingDeathEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		if (this.dropsList.isEmpty())
-			return;
-
-		if (!(event.getEntityLiving() instanceof EnderDragon dragon))
-			return;
-
-		CompoundTag tags = dragon.getPersistentData();
-		float difficulty = tags.getFloat(Strings.Tags.DIFFICULTY);
-		for (Drop drop : this.dropsList) {
-			drop.drop(dragon.level, dragon.position(), difficulty);
-		}
-	}
-
-	@SubscribeEvent
 	public void onLootTableLoad(LootTableLoadEvent event) {
-		if (!this.isEnabled())
+		if (!this.isEnabled() || !this.injectDefaultRewards)
 			return;
 
 		ResourceLocation name = event.getName();
--- src/main/java/insane96mcp/progressivebosses/module/wither/feature/RewardFeature.java
+++ src/main/java/insane96mcp/progressivebosses/module/wither/feature/RewardFeature.java
@@ -4,7 +4,6 @@ import insane96mcp.insanelib.base.Feature;
 import insane96mcp.insanelib.base.Label;
 import insane96mcp.insanelib.base.Module;
 import insane96mcp.progressivebosses.ProgressiveBosses;
-import insane96mcp.progressivebosses.classutils.Drop;
 import insane96mcp.progressivebosses.setup.Config;
 import insane96mcp.progressivebosses.setup.Strings;
 import net.minecraft.nbt.CompoundTag;
@@ -16,23 +15,16 @@ import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Label(name = "Rewards", description = "Bonus Experience and Drops")
 public class RewardFeature extends Feature {
 
 	private final ForgeConfigSpec.ConfigValue<Double> bonusExperienceConfig;
-	private final ForgeConfigSpec.ConfigValue<List<? extends String>> dropsListConfig;
-
-	private static final List<String> dropsListDefault = Arrays.asList("progressivebosses:nether_star_shard,2,1,0.75,PER_DIFFICULTY,FLAT", "minecraft:ancient_debris,2,1,0.75,PER_DIFFICULTY,FLAT");
+	private final ForgeConfigSpec.ConfigValue<Boolean> injectDefaultRewardsConfig;
 
 	public double bonusExperience = 7.5d;
-	public ArrayList<Drop> dropsList;
+	public boolean injectDefaultRewards = true;
 
 	public RewardFeature(Module module) {
 		super(Config.builder, module);
@@ -40,21 +32,10 @@ public class RewardFeature extends Feature {
 		bonusExperienceConfig = Config.builder
 				.comment("How much more experience (percentage) will Wither drop per Difficulty. The percentage is additive (e.g. with this set to 200%, 7 withers spawned = 1400% more experience)")
 				.defineInRange("Bonus Experience per Difficulty", bonusExperience, 0.0, Double.MAX_VALUE);
-		dropsListConfig = Config.builder
-				.comment("""
-						A list of drops for the Withers. Entry format: item,amount,difficulty_required,chance,difficulty_mode,chance_mode
-						item: item id
-						amount: amount
-						difficulty_required: the amount of difficulty required for the item to drop, works differently based on mode
-						chance: chance for the drop to happen, between 0 and 1
-						difficulty_mode:
-						* MINIMUM: will try to drop the item when the difficulty matches or is higher
-						* PER_DIFFICULTY: will try to drop the item once per difficulty (e.g. at difficulty 10, difficulty required 3, there is the chance to drop the item, trying 7 times)
-						chance_mode:
-						* FLAT: chance is the percentage chance for the item to drop if the difficulty criteria matches
-						* SCALING: each point of difficulty >= 'difficulty to drop the item' will be multiplied by the chance (e.g. chance 2% and difficulty 10, difficulty required 5, chance to drop the item will be chance * (difficulty - difficulty_required + 1) = 2% * (10 - 5 + 1) = 12%)
-						By default Withers have 75% chance per difficulty to drop 2 shard (So at difficulty 8, up to 16 shards can be dropped, 75% chance each 2).""")
-				.defineList("Drops", dropsListDefault, o -> o instanceof String);
+		injectDefaultRewardsConfig = Config.builder
+				.comment("If true default mod drops are added to the Wither.\n" +
+						"Note that replacing the Wither loot table (e.g. via DataPack) will automatically remove the Injected loot.")
+				.define("Injecet Default Loot", this.injectDefaultRewards);
 		Config.builder.pop();
 	}
 
@@ -62,7 +43,7 @@ public class RewardFeature extends Feature {
 	public void loadConfig() {
 		super.loadConfig();
 		this.bonusExperience = this.bonusExperienceConfig.get();
-		this.dropsList = Drop.parseDropsList(this.dropsListConfig.get());
+		this.injectDefaultRewards = this.injectDefaultRewardsConfig.get();
 	}
 
 	@SubscribeEvent
@@ -86,26 +67,8 @@ public class RewardFeature extends Feature {
 	}
 
 	@SubscribeEvent
-	public void onDeath(LivingDeathEvent event) {
-		if (!this.isEnabled())
-			return;
-
-		if (this.dropsList.isEmpty())
-			return;
-
-		if (!(event.getEntityLiving() instanceof WitherBoss wither))
-			return;
-
-		CompoundTag tags = wither.getPersistentData();
-		float difficulty = tags.getFloat(Strings.Tags.DIFFICULTY);
-		//for (Drop drop : this.dropsList) {
-		//	drop.drop(wither.level, wither.position(), difficulty);
-		//}
-	}
-
-	@SubscribeEvent
 	public void onLootTableLoad(LootTableLoadEvent event) {
-		if (!this.isEnabled())
+		if (!this.isEnabled() || !this.injectDefaultRewards)
 			return;
 
 		ResourceLocation name = event.getName();
