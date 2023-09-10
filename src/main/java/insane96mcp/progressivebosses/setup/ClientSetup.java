1e9c4595ceb1e761fb5ad334370d1e74b1a5d582
Insane96 <delvillano.alberto@gmail.com>
2023-09-10 21:00:20 +0200

    Corrupted soul sand in creative menu

--- src/main/java/insane96mcp/progressivebosses/setup/ClientSetup.java
+++ src/main/java/insane96mcp/progressivebosses/setup/ClientSetup.java
@@ -1,10 +1,14 @@
 package insane96mcp.progressivebosses.setup;
 
 import insane96mcp.progressivebosses.module.dragon.entity.LarvaRenderer;
+import insane96mcp.progressivebosses.module.wither.data.WitherStatsReloadListener;
 import insane96mcp.progressivebosses.module.wither.entity.PBWitherRenderer;
 import insane96mcp.progressivebosses.module.wither.entity.minion.WitherMinionRenderer;
 import insane96mcp.progressivebosses.module.wither.entity.skull.PBWitherSkullRenderer;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 
@@ -18,7 +22,18 @@ public class ClientSetup {
 
 	public static void creativeTabsBuildContents(final BuildCreativeModeTabContentsEvent event)
 	{
-		if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+		if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS)
+		{
+			//No need for the lvl 0 corruption
+			for (int i = 1; i < WitherStatsReloadListener.STATS_MAP.size(); i++) {
+				ItemStack stack = new ItemStack(PBItems.CORRUPTED_SOUL_SAND.get(), 1);
+				CompoundTag tag = new CompoundTag();
+				tag.putInt("lvl", i);
+				BlockItem.setBlockEntityData(stack, PBBlocks.CORRUPTED_SOUL_SAND_BLOCK_ENTITY.get(), tag);
+				event.accept(stack);
+			}
+		}
+		else if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
 		{
 			event.accept(PBItems.NETHER_STAR_SHARD.get());
 			event.accept(PBItems.ELDER_GUARDIAN_SPIKE.get());
