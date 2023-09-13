aa2432dccf19c5a5d7470b3c01d91081f58ac2a3
Insane96 <delvillano.alberto@gmail.com>
2023-09-13 10:13:33 +0200

    Strings

--- src/main/java/insane96mcp/progressivebosses/setup/PBItems.java
+++ src/main/java/insane96mcp/progressivebosses/setup/PBItems.java
@@ -10,7 +10,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class PBItems {
     public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ProgressiveBosses.MOD_ID);
 
-    public static final RegistryObject<Item> NETHER_STAR_SHARD = REGISTRY.register(Strings.Items.NETHER_STAR_SHARD, () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> ELDER_GUARDIAN_SPIKE = REGISTRY.register(Strings.Items.ELDER_GUARDIAN_SPIKE, () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> NETHER_STAR_SHARD = REGISTRY.register("nether_star_shard", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ELDER_GUARDIAN_SPIKE = REGISTRY.register("elder_guardian_spike", () -> new Item(new Item.Properties()));
     public static final RegistryObject<BlockItem> CORRUPTED_SOUL_SAND = REGISTRY.register("corrupted_soul_sand", () -> new BlockItem(PBBlocks.CORRUPTED_SOUL_SAND.get(), new Item.Properties()));
 }
