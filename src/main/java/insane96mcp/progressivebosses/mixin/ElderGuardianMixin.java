fefb4558ba01748137b328e900679c2d9acc480b
Insane96 <delvillano.alberto@gmail.com>
2023-11-12 11:02:43 +0100

    Fixed #153 (possible game freeze with Elder Guardians)

--- src/main/java/insane96mcp/progressivebosses/mixin/ElderGuardianMixin.java
+++ src/main/java/insane96mcp/progressivebosses/mixin/ElderGuardianMixin.java
@@ -1,19 +1,29 @@
 package insane96mcp.progressivebosses.mixin;
 
 import insane96mcp.progressivebosses.module.elderguardian.feature.AttackFeature;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.ElderGuardian;
+import net.minecraft.world.entity.monster.Guardian;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ElderGuardian.class)
-public class ElderGuardianMixin {
+public class ElderGuardianMixin extends Guardian {
+
+	public ElderGuardianMixin(EntityType<? extends Guardian> pEntityType, Level pLevel) {
+		super(pEntityType, pLevel);
+	}
 
 	@Inject(at = @At("HEAD"), method = "getAttackDuration()I", cancellable = true)
 	private void getAttackDuration(CallbackInfoReturnable<Integer> callback) {
 		if (!AttackFeature.shouldChangeAttackDuration((ElderGuardian) (Object) this))
 			return;
-		callback.setReturnValue(AttackFeature.getAttackDuration((ElderGuardian) (Object) this));
+		int attackDuration = AttackFeature.getAttackDuration((ElderGuardian) (Object) this);
+		if (this.clientSideAttackTime > attackDuration)
+			this.clientSideAttackTime = attackDuration;
+		callback.setReturnValue(attackDuration);
 	}
 }
